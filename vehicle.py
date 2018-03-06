e4862b7d85003d8150c1ad1b7f6289b28ad6ba37
jlakowski <jimlakowski@gmail.com>
2018-03-05 20:02:37 -0600

    added the vehicle class

---
+++ vehicle.py
@@ -0,0 +1,49 @@
+class Vehicle:
+    
+    def __init__(self, tag):
+        self.name = tag
+    
+    def setTag(self, tag):
+        self.tag = tag
+
+    def setPrice(self, price):
+        self.price = price
+
+    def setURL(self, url):
+        self.url = url
+
+    def setCondition(self, cond):
+        self.condition = cond
+    
+    def setCylinders(self, ncyl):
+        self.cylinders = ncyl
+    
+    def setDrive(self, drive):
+        self.drive = drive
+
+    def setFuel(self, fuel):
+        self.fuel = fuel
+        
+    def setMilage(self, odometer):
+        self.milage = odometer
+    
+    def setColor(self, color):
+        self.color = color
+    
+    def setTitleStatus(self, titleStat):
+        self.titleStatus = titleStat
+        
+    def setTranstype(self, transtype):
+        self.transtype = transtype
+
+    def setVehicletype(self, vehicletype):
+        self.vehicletype = vehicletype
+    
+    def setTimestamp(self, tstamp):
+        self.timestamp = tstamp
+    
+    def setID(self, idn):
+        self.idn = idn
+    
+    def setYear(self, year):
+        self.year = year
