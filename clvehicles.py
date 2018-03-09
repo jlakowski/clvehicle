042ead86330ed42033d02c454f1235abda24bbfd
jlakowski <jimlakowski@gmail.com>
2018-03-08 20:42:30 -0600

    added the vehicle object to clvehicle so now its all in 1 place!

--- clvehicles.py
+++ clvehicles.py
@@ -168,4 +168,52 @@ def main(argv):
 if __name__ == "__main__":
     main(sys.argv)
 
+class Vehicle:
+    
+    def __init__(self, tag):
+        self.name = tag
+    
+    def setTag(self, tag):
+        self.tag = tag
 
+    def setPrice(self, price):
+        self.price = price
+
+    def setURL(self, url):
+        self.url = url
+
+    def setCondition(self, cond):
+        self.condition = cond
+    
+    def setCylinders(self, ncyl):
+        self.cylinders = ncyl
+    
+    def setDrive(self, drive):
+        self.drive = drive
+
+    def setFuel(self, fuel):
+        self.fuel = fuel
+        
+    def setMilage(self, odometer):
+        self.milage = odometer
+    
+    def setColor(self, color):
+        self.color = color
+    
+    def setTitleStatus(self, titleStat):
+        self.titleStatus = titleStat
+        
+    def setTranstype(self, transtype):
+        self.transtype = transtype
+
+    def setVehicletype(self, vehicletype):
+        self.vehicletype = vehicletype
+    
+    def setTimestamp(self, tstamp):
+        self.timestamp = tstamp
+    
+    def setID(self, idn):
+        self.idn = idn
+    
+    def setYear(self, year):
+        self.year = year
